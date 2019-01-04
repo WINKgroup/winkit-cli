2652db03adc6fc508dfc17d64c3a6276ec7ba5db
Franco Cosimo <franco.cosimo95@gmail.com>
2019-01-04 10:42:36 +0100

    new version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {createPlugin, updatePlugin, deletePlugin, supportedPlugins} = require('./
 const program = require('commander');
 const fs = require('fs');
 
-const version = '1.0.5';
+const version = '1.0.6';
 
 function addPlugin(plugin, p) {
     if (fs.existsSync(`${__dirname}/plugins/${plugin}/index.js`)) {
